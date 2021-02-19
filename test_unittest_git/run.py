d3fdc37d7d50e9980a59eb79471458aa5b123e7d
chenci <CcMac>
2021-02-19 19:20:26 +0800

    新增os模块

--- test_unittest_git/run.py
+++ test_unittest_git/run.py
@@ -7,10 +7,9 @@
 
 import sys
 import os
-# curPath = os.path.abspath(os.path.dirname(__file__))
-# rootPath = os.path.split(curPath)[0]
-# sys.path.append(rootPath)
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 
 import unittest
 import time
