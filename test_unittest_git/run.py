175e2be956daa171139610236c9aa4b71dfe93f9
chenci <CcMac>
2021-02-19 19:19:35 +0800

    修改run

--- test_unittest_git/run.py
+++ test_unittest_git/run.py
@@ -7,9 +7,10 @@
 
 import sys
 import os
-curPath = os.path.abspath(os.path.dirname(__file__))
-rootPath = os.path.split(curPath)[0]
-sys.path.append(rootPath)
+# curPath = os.path.abspath(os.path.dirname(__file__))
+# rootPath = os.path.split(curPath)[0]
+# sys.path.append(rootPath)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 import unittest
 import time
